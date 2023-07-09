75e75490e696e3a0f1b558227213e4d5bf425f29
Partizanzero <ElenaD81@list.ru>
2023-07-09 17:20:55 +0400

    Add files via upload

--- task_4/task_4.js
+++ task_4/task_4.js
@@ -3,7 +3,7 @@
 const inputWidth = document.getElementById("width");
 // Ищем поле, в которое будем вводить высоту
 const inputHeight = document.getElementById("height");
-// Ищем кнопку, по нажатии на которую будет запрос
+// Ищем кнопку, по нажатии на которую будет запрос к серверу
 const submitButton = document.querySelector("button");
 // Ищем ноду для вставки процесса загрузки или ошибок
 const outputSpan = document.querySelector("span");
@@ -13,13 +13,13 @@ const photosContainer = document.querySelector("div");
 //Вешаем обработчик на кнопку для запроса
 submitButton.addEventListener('click', submitButtonHandle);
 
-//вспомагательная функция для вывода сообщений о загрузке и ошибках в HTML-документ
+//вспомогательная функция для вывода сообщений о загрузке и ошибках в HTML-документ
 function write(text) {
     outputSpan.innerHTML = text;
 }
 
 //функция, которая проверяет число, введенное в <input> и, если оно попадает в указанный диапазон, то
-//запускает фукцию, которая будет делать запрос к удаленному серверу через XHR API для получения фото
+//будет делать запрос к удаленному серверу для получения фото, используя fetch 
 
 function submitButtonHandle() {
 
@@ -34,7 +34,7 @@ function submitButtonHandle() {
     }
 
     //делаем запрос за данными
-    fetch('https://picsum.photos/${width}/${height}')
+    fetch(`https://picsum.photos/${width}/${height}`)
         .then((response) => response.url)
         .then((result) => {
             loadPhoto(result);
@@ -49,8 +49,8 @@ function submitButtonHandle() {
 //callback - функция для сборки фото и вставки на страницу
 function loadPhoto(photoUrl) {
     const cardBlock = `
-<img src="src="${photoUrl}" style="margin-right: 30px" />`;
-    //вставляем все полученные фото в <div>, который у нас получен в const photosContainer
+<img src="${photoUrl}" style="margin-right: 30px" />`;
+    //вставляем полученное фото в <div>, который у нас получен в const photosContainer
     photosContainer.innerHTML = cardBlock;
 }
 
